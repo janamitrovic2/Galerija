6fda22a89b99dfff60cdeb7ddf5873e62335e2c6
Jana Mitrovic <mitrovicmitrovic1234@gmail.com>
2024-06-17 00:20:58 +0200

    front 1.0.1

--- pages/galerija.php
+++ pages/galerija.php
@@ -1,20 +1,10 @@
-<?php
-    if (isset($_POST['btnSubmit']) )
-    {
-        echo "Uspešno smo pokupili podatke iz formulara na serveru GET metodom<br>";
-        echo "Podaci <br> Email: ".$_GET['email']."</br> Password: " .$_GET['password'];
-    }
-    if(isset($_POST['btnReset'])){
-        echo "klose";
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="..styles/styles.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Galerija slika</title>
 </head>
@@ -45,10 +35,35 @@
 
         </div>
 
-        <div class="container-sm my-5 border">
+        <div class="container-sm my-5 py-5 border text-center">
+            <div class="">
+                <h1>Galerija uploadovanih slika</h1>
+            </div>
+            <div class="">
+                <div id="galerija" class="carousel slide" data-bs-ride="carousel">
 
-            
+                    <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+                    </div>
 
+                    <div class="carousel-inner">
+                        <?php
+                        $image_files = glob("uploads/*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+                        $active = true;
+                        foreach ($image_files as $index => $image) {
+                            echo '<div class="carousel-item ' . ($active ? 'active' : '') . '">';
+                            echo '<img src="' . $image . '" class="d-block w-100" alt="Slide ' . ($index + 1) . '">';
+                            echo '</div>';
+                            $active = false;
+                        }
+                        ?>
+                    </div>
+
+                    
+                </div>
+            </div>
         </div>
     </div>
     <script src="script/script.js"></script>
