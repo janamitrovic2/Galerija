3dbe1d870d42ddbba021a41f88f93bb42f58114f
Jana Mitrovic <mitrovicmitrovic1234@gmail.com>
2024-06-17 18:35:38 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+    $target_dir="uploads/";
+    if(!file_exists($target_dir)){
+        mkdir($target_dir,0777);
+    }
+    ## ukoliko ne postoji folder napravi ga
+                            ## files["name"]["name"] vraca puno ime fajla
+                            // echo(($_FILES["slika"]["name"]))."<br>"; opravdanje.jpg
+                            // echo(basename($_FILES["slika"]["name"]))."<br>"; opradanje.jpg
+                            // echo($target_file)."<br>";
+                            // echo($imageFileType); jpg
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,44 +23,44 @@
 </head>
 <body>
     <div>
+        <!-- HERO -->
         <div class="container-fluid bg-primary text-center text-light p-5">
 
             <div class="mb-5">
-
                 <h1 class="display-1">Galerija slika</h1>
-
             </div>
 
             <div class="text-light mt-5">
-
                 <nav class="navbar navbar-expand-sm justify-content-center">
                     <ul class="nav-tabs navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link text-light" href="#">Pocetna</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="/pages/galerija.php">Galerija</a>
+                            <a class="nav-link text-light" href="galerija.php">Galerija</a>
                         </li>
                     </ul>
                 </nav>
-
             </div>
 
         </div>
 
+        <!-- BODY -->
         <div class="container-sm my-5 py-5 border">
 
             <div class="text-center">
+
                 <div>
                     <h1>Unesi sliku!</h1>
                 </div>
-                <div class="mt-4">
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post" enctype="multipart/form-data">
-                        
+
+                <div class="my-4">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+
                         <div class="py-4">
                             <div class="row justify-content-center mb-4">
                                 <div class="col-md-5 col-12">
-                                    <input class="form-control " type="file" name="slika" id="slika" placeholder="Izaberite fajl...">
+                                    <input class="form-control" type="file" name="slika" id="slika" placeholder="Izaberite fajl...">
                                 </div>
                             </div>
                             <div class="row justify-content-center">
@@ -56,9 +69,8 @@
                                     <div class="preview" id="preview"></div>
                                 </div>
                             </div>
-                            
                         </div>
-                        
+
                         <div class="mb-4">
                             <input class="w-25 mx-auto form-control" type="text" name="naziv" id="naziv" placeholder="Naziv slike">
                         </div>
@@ -70,66 +82,75 @@
                             <div class="col-sm">
                                 <button type="reset" name="btnReset" class="btn btn-light">Reset</button>
                             </div>
-
                         </div>
+
                     </form>
                 </div>
-                
+
+                <div class="">
+
+                    <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btnSubmit"])) 
+                    {
+                        if(isset($_POST["naziv"]) && htmlspecialchars($_POST["naziv"]!=""))
+                        {
+
+
+                            $ime= $_POST['naziv']; // uzima ime iz text box
+                            $_FILES['slika']['name']=$ime.'.'.strtolower(pathinfo($_FILES['slika']['name'], PATHINFO_EXTENSION)); //promena imena slike
+                            $target_file = $target_dir.basename($_FILES["slika"]["name"]); # puna putanja do uploadovanog fajla
+                            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION)); #ekstenzija tipa - jpg
+                            $uploadOk = 1;
+
+
+                            // da li postoji slika
+                            if (file_exists($target_file)) {
+                                echo "<div class='alert alert-danger'>Fajl s s istim imenom vec postoji.</div>";
+                                $uploadOk = 0;
+                            }
+
+                            // provera da li je slika, da li prelazi 5MB i da li je zeljenog formata pristupom preko $_FILES['slika']
+                            $check = getimagesize($_FILES["slika"]["tmp_name"]);
+                            if ($check !== false) 
+                            {
+                                if ($_FILES["slika"]["size"] > 5000000) 
+                                {
+                                    echo "<div class='alert alert-danger'>Slika prelazi 5MB.</div>";
+                                    $uploadOk = 0;
+                                }
+                                if($imageFileType!= "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif")
+                                //if(!in_array($_FILES["slika"]["type"],['image/png','image/jpg','image/jpeg','image/giff'])
+                                {
+                                    echo "<div class='alert alert-danger'>Slika nije jpg/jpeg/png/gif formata.</div>";
+                                    $uploadOk = 0;
+                                }
+                            } else {
+                                echo "<div class='alert alert-danger'>Niste uneli sliku</div>";
+                                $uploadOk = 0;
+                            }
+
+                            if ($uploadOk != 0 ) 
+                            {
+                                if (move_uploaded_file($_FILES["slika"]["tmp_name"], $target_file)) {
+                                    echo "<div class='alert alert-success'>Slika". htmlspecialchars(basename($_FILES["slika"]["name"])). " je uploadovana.</div>";
+                                } else {
+                                    echo "<div class='alert alert-danger'>Sorry, your file was not uploaded.</div>";
+                                }
+                            } 
+
+                        }
+                        else
+                            echo "<div class='alert alert-danger'>Unesite novo ime vase slike</div>";
+                    }
+                    ?>
+                </div>
+
             </div>
 
         </div>
     </div>
+
     <script src="script/script.js"></script>
-    <?php
-        
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btnSubmit"])) {
-            $target_dir = "uploads/";
-            $target_file = $target_dir . basename($_FILES["slika"]["name"]);
-            $uploadOk = 1;
-            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        
-            // Check if image file is a actual image or fake image
-            $check = getimagesize($_FILES["slika"]["tmp_name"]);
-            if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
-            }
-        
-            // Check if file already exists
-            if (file_exists($target_file)) {
-                echo "Sorry, file already exists.";
-                $uploadOk = 0;
-            }
-        
-            // Check file size
-            if ($_FILES["slika"]["size"] > 500000) {
-                echo "Sorry, your file is too large.";
-                $uploadOk = 0;
-            }
-        
-            // Allow certain file formats
-            $allowed_types = array('jpg', 'jpeg', 'png', 'gif');
-            if(!in_array($imageFileType, $allowed_types)) {
-                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                $uploadOk = 0;
-            }
-        
-            // Check if $uploadOk is set to 0 by an error
-            if ($uploadOk == 0) {
-                echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
-            } else {
-                if (move_uploaded_file($_FILES["slika"]["tmp_name"], $target_file)) {
-                    echo "The file ". htmlspecialchars(basename($_FILES["slika"]["name"])). " has been uploaded.";
-                } else {
-                    echo "Sorry, there was an error uploading your file.";
-                }
-            }
-        }
-    ?>
-        
+    
 </body>
 </html>
